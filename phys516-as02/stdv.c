0a3dc899b269887714dc2e90a7d68f099040de23
jasonwhj21 <67030932+jasonwhj21@users.noreply.github.com>
2023-01-21 14:30:58 -0800

    Add files via upload

---
+++ phys516-as02/stdv.c
@@ -0,0 +1,27 @@
+/* Monte Carlo integration of PI by sample mean */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define Nseed 100
+int main() {
+  double x, pi, sum = 0.0, pi_av = 0.0, pi2_av = 0.0, stdv;;
+  int try, ntry, outer;
+  printf("Input the number of MC trials\n");
+  scanf("%d",&ntry);
+  srand((unsigned)time((long *)0));
+  for (outer=0; outer<Nseed; outer++) {
+    sum = 0.0
+    for (try=0; try<ntry; try++) {
+      x = rand()/(double)RAND_MAX;
+      sum += 4.0/(1.0 + x*x);
+    }
+    pi = sum/ntry;
+    pi_av += pi;
+    pi2_av += pi*pi;
+  }
+  pi_av /= Nseed;
+  pi2_av /= Nseed;
+  stdv = sqrt(pi2_av-pi_av*pi_av)
+  printf("Ntry & stdv: %d\t%e\n = %f\n", stdv);
+  return 0;
+}
